6e9a035ded4766dfe5264b167ca4d6ed4de28d22
Jacek Zapart <jacek.zapart@asideas.de>
2016-02-12 10:25:48 +0100

    Empty Implementation - just project structure

---
+++ DLSplashModule/DLSplashModule.h
@@ -0,0 +1,13 @@
+//
+//  DLSplashModule.h
+//  DLSplashModule
+//
+//  Created by Jacek Zapart on 12.02.2016.
+//  Copyright © 2016 DreamLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DLSplashModule : NSObject
+
+@end
