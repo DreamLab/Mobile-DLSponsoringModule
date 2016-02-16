3b39deb7e67c85a397f974e55a225b407214ec3c
Konrad Kierys <konrad.kierys@asideas.de>
2016-02-16 12:21:04 +0100

    added comment; refactored code

--- DLSplashModuleTests/DLSplashAdTests.m
+++ DLSplashModuleTests/DLSplashAdTests.m
@@ -20,7 +20,8 @@
 
 @implementation DLSplashAdTests
 
-- (void)setUp {
+- (void)setUp
+{
     [super setUp];
 
     self.correctJSONData = [DLTestingHelper dataFromJSONFileNamed:@"CorrectJsonData"];
@@ -28,7 +29,8 @@
     self.emptyJSONData = [DLTestingHelper dataFromJSONFileNamed:@"EmptyJsonData"];
 }
 
-- (void)tearDown {
+- (void)tearDown
+{
     self.correctJSONData = nil;
     self.wrongJSONData = nil;
     self.emptyJSONData = nil;
@@ -36,13 +38,15 @@
     [super tearDown];
 }
 
-- (void)testCreatingSplashAd_givenCorrectJSONData_splashAdShouldNotBeNil {
+- (void)testCreatingSplashAd_givenCorrectJSONData_splashAdShouldNotBeNil
+{
     DLSplashAd *splashAd = [[DLSplashAd alloc] initWithJSONData:self.correctJSONData];
 
     XCTAssertNotNil(splashAd, @"Splash Ad should not be nil");
 }
 
-- (void)testCreatingSplashAd_givenCorrectJSONData_imageUrlIsNotNilAndCorrect {
+- (void)testCreatingSplashAd_givenCorrectJSONData_imageUrlIsNotNilAndCorrect
+{
     DLSplashAd *splashAd = [[DLSplashAd alloc] initWithJSONData:self.correctJSONData];
 
     XCTAssertNotNil(splashAd.imageURL, @"SplashAd ImageURL should not be nil");
@@ -50,32 +54,37 @@
     XCTAssertEqualObjects(splashAd.imageURL, testURL, @"SplashAd ImageURL should be correct");
 }
 
-- (void)testCreatingSplashAd_givenCorrectJSONData_imageWidthIsCorrect {
+- (void)testCreatingSplashAd_givenCorrectJSONData_imageWidthIsCorrect
+{
     DLSplashAd *splashAd = [[DLSplashAd alloc] initWithJSONData:self.correctJSONData];
 
     XCTAssertEqual(splashAd.imageWidth, 90, @"SplashAd imageWidth should be correct");
 }
 
-- (void)testCreatingSplashAd_givenCorrectJSONData_imageHeightIsCorrect {
+- (void)testCreatingSplashAd_givenCorrectJSONData_imageHeightIsCorrect
+{
     DLSplashAd *splashAd = [[DLSplashAd alloc] initWithJSONData:self.correctJSONData];
 
     XCTAssertEqual(splashAd.imageHeight, 60, @"SplashAd imageHeight should be correct");
 }
 
-- (void)testCreatingSplashAd_givenCorrectJSONData_adTextIsNotNilAndCorrect {
+- (void)testCreatingSplashAd_givenCorrectJSONData_adTextIsNotNilAndCorrect
+{
     DLSplashAd *splashAd = [[DLSplashAd alloc] initWithJSONData:self.correctJSONData];
 
     XCTAssertNotNil(splashAd.text, @"SplashAd text should not be nil");
     XCTAssertEqualObjects(splashAd.text, @"Partner aplikacji", @"SplashAd text should be correct");
 }
 
-- (void)testCreatingSplashAd_givenCorrectJSONData_timeIsCorrect {
+- (void)testCreatingSplashAd_givenCorrectJSONData_timeIsCorrect
+{
     DLSplashAd *splashAd = [[DLSplashAd alloc] initWithJSONData:self.correctJSONData];
 
     XCTAssertEqual(splashAd.time, 5, @"SplashAd time should be correct");
 }
 
-- (void)testCreatingSplashAd_givenCorrectJSONData_auditUrlIsNotNilAndCorrect {
+- (void)testCreatingSplashAd_givenCorrectJSONData_auditUrlIsNotNilAndCorrect
+{
     DLSplashAd *splashAd = [[DLSplashAd alloc] initWithJSONData:self.correctJSONData];
 
     XCTAssertNotNil(splashAd.auditURL, @"SplashAd auditURL should not be nil");
@@ -83,7 +92,8 @@
     XCTAssertEqualObjects(splashAd.auditURL, testURL, @"SplashAd auditURL should be correct");
 }
 
-- (void)testCreatingSplashAd_givenCorrectJSONData_audit2UrlIsNotNilAndCorrect {
+- (void)testCreatingSplashAd_givenCorrectJSONData_audit2UrlIsNotNilAndCorrect
+{
     DLSplashAd *splashAd = [[DLSplashAd alloc] initWithJSONData:self.correctJSONData];
 
     XCTAssertNotNil(splashAd.audit2URL, @"SplashAd audit2URL should not be nil");
@@ -91,7 +101,8 @@
     XCTAssertEqualObjects(splashAd.audit2URL, testURL, @"SplashAd audit2URL should be correct");
 }
 
-- (void)testCreatingSplashAd_givenCorrectJSONData_clickUrlIsNotNilAndCorrect {
+- (void)testCreatingSplashAd_givenCorrectJSONData_clickUrlIsNotNilAndCorrect
+{
     DLSplashAd *splashAd = [[DLSplashAd alloc] initWithJSONData:self.correctJSONData];
 
     XCTAssertNotNil(splashAd.clickURL, @"SplashAd clickURL should not be nil");
@@ -99,19 +110,22 @@
     XCTAssertEqualObjects(splashAd.clickURL, testURL, @"SplashAd clickURL should be correct");
 }
 
-- (void)testCreatingSplashAd_givenCorrectJSONData_versionIsCorrect {
+- (void)testCreatingSplashAd_givenCorrectJSONData_versionIsCorrect
+{
     DLSplashAd *splashAd = [[DLSplashAd alloc] initWithJSONData:self.correctJSONData];
 
     XCTAssertEqual(splashAd.version, 1234512, @"SplashAd version should be correct");
 }
 
-- (void)testCreatingSplashAd_givenCorruptedJSONData_splashAdShouldBeNil {
+- (void)testCreatingSplashAd_givenCorruptedJSONData_splashAdShouldBeNil
+{
     DLSplashAd *splashAd = [[DLSplashAd alloc] initWithJSONData:self.wrongJSONData];
 
     XCTAssertNil(splashAd, @"Splash Ad should be nil");
 }
 
-- (void)testCreatingSplashAd_givenEmptyJSONData_splashAdShouldBeNil {
+- (void)testCreatingSplashAd_givenEmptyJSONData_splashAdShouldBeNil
+{
     DLSplashAd *splashAd = [[DLSplashAd alloc] initWithJSONData:self.emptyJSONData];
 
     XCTAssertNil(splashAd, @"Splash Ad should be nil");
--- DLSplashModuleTests/DLTestingHelper.h
+++ DLSplashModuleTests/DLTestingHelper.h
@@ -8,8 +8,18 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  Helper for Unit Tests
+ */
 @interface DLTestingHelper : NSObject
 
+/**
+ *  Reads content of file in unit test bundle
+ *
+ *  @param fileName Name of the file
+ *
+ *  @return Content of the file as NSData
+ */
 + (NSData *)dataFromJSONFileNamed:(NSString *)fileName;
 
 @end
