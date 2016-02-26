a7000adab1b6c56d420e14d6ae9ac8ebaa53c252
Jacek Zapart <jacek.zapart@asideas.de>
2016-02-26 16:02:25 +0100

    Comment in unversal.sh script

--- bin/universal.sh
+++ bin/universal.sh
@@ -1,1 +1,4 @@
+# To create dependency required by Demo project target 'DLSplashDemoPods' call:
+# CONFIGURATION_BUILD_DIR='../Pod' bin/universal.sh
+#
 xcodebuild -project DLSplashModule.xcodeproj -scheme DLSplashModuleUniversal -configuration Release clean build CONFIGURATION_BUILD_DIR=$CONFIGURATION_BUILD_DIR
